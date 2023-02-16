7c7fd8237b06ba7e12cbe73edbff099edbb4c593
TinAungLinn <hp948074@gmail.com>
2023-02-16 21:47:49 +0630

    finish Invoice App

--- js/invoice.js
+++ js/invoice.js
@@ -25,6 +25,7 @@ const services = [
 
 //Selectors
 
+const app = document.querySelector("#app");
 const invoiceForm = document.querySelector("#invoiceForm");
 const selectService = document.querySelector("#selectService");
 const quantity = document.querySelector("#quantity");
@@ -32,6 +33,7 @@ const lists = document.querySelector("#lists");
 const subTotal = document.querySelector("#subTotal");
 const tax = document.querySelector("#tax");
 const total = document.querySelector("#total");
+const listTable = document.querySelector("#listTable");
 
 //Process (Tesks)
 
@@ -39,26 +41,44 @@ const total = document.querySelector("#total");
 
 const createTr = (service, quantity) => {
   const tr = document.createElement("tr");
+  tr.setAttribute("service-id", service.id);
   tr.classList.add("list");
   const total = service.price * quantity;
   tr.innerHTML = `
-  <td>${service.title}</td>
-  <td class=" text-end">${quantity}</td>
+  <td class=" d-flex justify-content-between">
+  ${service.title}
+  <i class="bi bi-trash3 text-danger del-btn"></i>
+  </td>
+  <td class=" text-end list-quantity">${quantity}</td>
   <td class=" text-end">${service.price}</td>
   <td class=" text-end list-total">${total}</td>
   `;
   return tr;
 };
 
-const findTax = (amount, percentage = 5) => {
+const calculateTax = (amount, percentage = 5) => {
   return amount * (percentage / 100);
 };
 
 const findTotal = () => {
   const listTotal = document.querySelectorAll(".list-total");
-  const subTotal = [...document.querySelectorAll(".list-total")].reduce((pv,cv) => pv + parseFloat(cv.innerText), 0);
-  console.log(subTotal);
-}
+  const subTotalCalculated = [...listTotal].reduce(
+    (pv, cv) => pv + parseFloat(cv.innerText),
+    0
+  );
+  // console.log(subTotal);
+  subTotal.innerText = subTotalCalculated;
+  tax.innerText = calculateTax(subTotalCalculated);
+  total.innerText = subTotalCalculated + calculateTax(subTotalCalculated);
+};
+
+const showTable = () => {
+  if (lists.children.length) {
+    listTable.classList.remove("d-none");
+  } else {
+    listTable.classList.add("d-none");
+  }
+};
 
 //Service Option Loop
 
@@ -85,9 +105,31 @@ invoiceForm.addEventListener("submit", (event) => {
     (service) => service.id == selectService.value
   );
 
-  lists.append(createTr(selectedService, quantity.valueAsNumber));
+  const isExistedService = [...lists.children].find(
+    (el) => el.getAttribute("service-id") == selectedService.id
+  );
+
+  if (isExistedService) {
+    const existedQuantity = isExistedService.querySelector(".list-quantity");
+    existedQuantity.innerText =
+      parseFloat(existedQuantity.innerText) + quantity.valueAsNumber;
+    isExistedService.querySelector(".list-total").innerText =
+      existedQuantity.innerText * selectedService.price;
+  } else {
+    lists.append(createTr(selectedService, quantity.valueAsNumber));
+  }
 
   findTotal();
 
   invoiceForm.reset();
+  showTable();
+});
+
+app.addEventListener("click", (event) => {
+  const currentElement = event.target;
+  if (currentElement.classList.contains("del-btn")) {
+    currentElement.closest("tr").remove();
+    findTotal();
+    showTable();
+  }
 });
