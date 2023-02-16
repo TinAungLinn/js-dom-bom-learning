a076fdeaae8f4e7960f295959eb2ba9bbdca108b
TinAungLinn <hp948074@gmail.com>
2023-02-16 19:31:09 +0630

    ivoice app update

--- js/invoice.js
+++ js/invoice.js
@@ -28,18 +28,66 @@ const services = [
 const invoiceForm = document.querySelector("#invoiceForm");
 const selectService = document.querySelector("#selectService");
 const quantity = document.querySelector("#quantity");
+const lists = document.querySelector("#lists");
+const subTotal = document.querySelector("#subTotal");
+const tax = document.querySelector("#tax");
+const total = document.querySelector("#total");
 
 //Process (Tesks)
+
+//Function
+
+const createTr = (service, quantity) => {
+  const tr = document.createElement("tr");
+  tr.classList.add("list");
+  const total = service.price * quantity;
+  tr.innerHTML = `
+  <td>${service.title}</td>
+  <td class=" text-end">${quantity}</td>
+  <td class=" text-end">${service.price}</td>
+  <td class=" text-end list-total">${total}</td>
+  `;
+  return tr;
+};
+
+const findTax = (amount, percentage = 5) => {
+  return amount * (percentage / 100);
+};
+
+const findTotal = () => {
+  const listTotal = document.querySelectorAll(".list-total");
+  const subTotal = [...document.querySelectorAll(".list-total")].reduce((pv,cv) => pv + parseFloat(cv.innerText), 0);
+  console.log(subTotal);
+}
 
 //Service Option Loop
 
 services.forEach((service) =>
-  selectService.append(new Option(service.title, service.price))
+  selectService.append(new Option(service.title, service.id))
 );
 
 //Data collect from from
 
 invoiceForm.addEventListener("submit", (event) => {
   event.preventDefault();
-  
+  // console.log(
+  //   selectService.value,
+  //   selectService.options[selectService.selectedIndex].innerText,
+  //   quantity.valueAsNumber,
+  // )
+  // console.log(
+  //   selectService.value,
+  //   quantity.valueAsNumber,
+  //   services.find((service) => service.id == selectService.value)
+  // );
+
+  const selectedService = services.find(
+    (service) => service.id == selectService.value
+  );
+
+  lists.append(createTr(selectedService, quantity.valueAsNumber));
+
+  findTotal();
+
+  invoiceForm.reset();
 });
